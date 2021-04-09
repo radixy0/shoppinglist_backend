3fa75345d5cfeaaee2eed634b735aca25b6585ae
radixy0 <80622923+radixy0@users.noreply.github.com>
2021-04-10 01:20:36 +0200

    test commit

--- src/main/java/com/shoppinglist/backend/Item.java
+++ src/main/java/com/shoppinglist/backend/Item.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
+//a
 @Entity
 public class Item {
     private @Id @GeneratedValue Long id;
